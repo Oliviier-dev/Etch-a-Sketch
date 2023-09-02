6947b72d111fdb9a1657347b7619a0bfde3989f9
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-09-02 10:00:08 +0200

    Handled the Dom manipulation

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let cells;
+let gridSize = prompt("enter grid size");
+
+let button = document.querySelector('.btn');
+
+button.addEventListener('click', function(){
+    cells = document.querySelectorAll('.cell');
+    cells.forEach(cell =>{
+        cell.style.backgroundColor = 'white';
+    })
+
+})
+
 function createGrids(gridSize){
     let container = document.querySelector('.container');
+
     for (let i = 0; i < gridSize; i++){
         let row = document.createElement('div');
         row.classList.add('row');
@@ -10,6 +24,15 @@ function createGrids(gridSize){
             row.appendChild(cell);
         }
     }
+
+    cells = document.querySelectorAll('.cell');
+    cells.forEach(cell =>{
+        cell.addEventListener('mouseenter', function(){
+            cell.style.backgroundColor = 'black';
+            // console.log("Ok");
+        })
+    })
+
 }
 
-createGrids(32)
+createGrids(gridSize);
